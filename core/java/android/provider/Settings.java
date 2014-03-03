11e44c021624fcd2d92591263679deffddc7035f
GwonHyeok <kh4975@naver.com>
2014-03-03 11:43:07 +0000

    base: Add toast animations [1/2]
    * Re-adapt toast window animations for Nougat by westcripp
    
    Change-Id: Ic4d1b573b8ed5c657e8e9b226c28944d6a0895ec

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3932,6 +3932,13 @@ public final class Settings {
         public static final String ANIM_TILE_INTERPOLATOR = "anim_tile_interpolator";
 
         /**
+         * Toast animations
+         *
+         * @hide
+         */
+        public static final String TOAST_ANIMATION = "toast_animation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
