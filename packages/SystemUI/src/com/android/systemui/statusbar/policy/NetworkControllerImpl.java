9b832bb6399e6c566dbd3e1af2ca4dfac367a186
qjohn <townex22@gmail.com>
2016-06-11 10:56:31 +0700

    Add Wi-Fi SSID display option to Adv Wifi settings[2/2]

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
@@ -235,6 +235,10 @@ public class NetworkControllerImpl extends BroadcastReceiver
         return mWifiSignalController.getState().level;
     }
 
+    public String getConnectedWifiSsid() {
+        return mWifiSignalController.getWifiSsid();
+    }
+
     @Override
     public AccessPointController getAccessPointController() {
         return mAccessPoints;
@@ -955,6 +959,10 @@ public class NetworkControllerImpl extends BroadcastReceiver
                 }
             }
             return null;
+        }
+
+        public String getWifiSsid() {
+            return mCurrentState.ssid;
         }
 
         @VisibleForTesting
