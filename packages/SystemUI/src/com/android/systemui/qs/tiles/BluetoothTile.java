0584a11483f47cdea8895c999154431874e61041
xyyx <xyyx@mail.ru>
2016-10-04 13:38:38 +0800

    QS Tile: add BT easy toggle [1/2]
    Change-Id: I7256018c8d6560526242f3bf6c1bfbbd062db5dc

--- packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
@@ -87,14 +87,21 @@ public class BluetoothTile extends QSTile<QSTile.BooleanState>  {
 
     @Override
     protected void handleClick() {
-        if (!mController.canConfigBluetooth()) {
-            mHost.startActivityDismissingKeyguard(new Intent(Settings.ACTION_BLUETOOTH_SETTINGS));
-            return;
-        }
-        showDetail(true);
-        if (!mState.value) {
-            mState.value = true;
-            mController.setBluetoothEnabled(true);
+        boolean easyToggle = isBtEasyToggleEnabled();
+        if (easyToggle) {
+            final boolean isEnabled = (Boolean)mState.value;
+            MetricsLogger.action(mContext, getMetricsCategory(), !isEnabled);
+               mController.setBluetoothEnabled(!isEnabled);
+        } else {
+            if (!mController.canConfigBluetooth()) {
+                mHost.startActivityDismissingKeyguard(new Intent(Settings.ACTION_BLUETOOTH_SETTINGS));
+                return;
+            }
+            showDetail(true);
+            if (!mState.value) {
+                mState.value = true;
+                mController.setBluetoothEnabled(true);
+            }
         }
     }
 
@@ -159,6 +166,11 @@ public class BluetoothTile extends QSTile<QSTile.BooleanState>  {
                 R.string.accessibility_quick_settings_open_settings, getTileLabel());
         state.expandedAccessibilityClassName = Button.class.getName();
         state.minimalAccessibilityClassName = Switch.class.getName();
+    }
+
+    public boolean isBtEasyToggleEnabled() {
+        return Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.QS_BT_EASY_TOGGLE, 0) == 1;
     }
 
     @Override
