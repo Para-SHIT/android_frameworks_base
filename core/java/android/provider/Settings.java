a991e4a4b7799dc8e2dd6054fc6b029dabf9c4a2
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-12-10 16:32:31 +0100

    base: show battery info on lockscreen when charging [1/2]
    - negociated max. charging current
    - negociated max. charging voltage
    - current battery temperature
    - Formatting improvements by nathanchance and xyyx
    
    Change-Id: Iac2a2194d61612a7af23ca49a4f1c0930d8f8079
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3999,6 +3999,12 @@ public final class Settings {
         public static final String HIDE_LOCKSCREEN_ALARM = "hide_lockscreen_alarm";
 
         /**
+         * Whether to show the battery info on the lockscreen while charging
+         * @hide
+         */
+        public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
