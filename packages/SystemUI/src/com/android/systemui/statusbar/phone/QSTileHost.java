f43185eb5ebc66fe015d88406f57f0ab40322629
qjohn <townex22@gmail.com>
2017-10-28 19:04:25 +0700

    Revert "QS: Add Sound tile"
    This reverts commit b2cd01f75c033f8bea575468cc29e016cd32d34b.
    
    Change-Id: I4f63b62ab5aecaced58cd2190d91d2792cca5f48

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -65,7 +65,6 @@ import com.android.systemui.qs.tiles.ParaSHITTile;
 import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
-import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.UserTile;
@@ -471,7 +470,6 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("parashit")) return new ParaSHITTile(this);
         else if (tileSpec.equals("reboot")) return new RebootTile(this);
         else if (tileSpec.equals("screenshot")) return new ScreenshotTile(this);
-        else if (tileSpec.equals("sound")) return new SoundTile(this);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(this,tileSpec);
